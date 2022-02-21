b2ca60c5117b92b88fd59023841fd6510a8f5d6e
MuhammadAli <77521737+MukhammadAli9723@users.noreply.github.com>
2022-02-21 10:32:52 +0500

    Initial Commit

---
+++ AlamofireMasterClass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireMasterClass
+//
+//  Created by Muhammadali Yulbarsbekov on 21/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
